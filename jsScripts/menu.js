11fa782e7a6305153cbfd8266d8c3a45e401e9b5
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-04-11 12:58:38 +0500

    Add blur effect for the menu modal

--- jsScripts/menu.js
+++ jsScripts/menu.js
@@ -1,13 +1,16 @@
 const menuBtn = document.querySelector('.menu-icon');
 const menu = document.querySelector('.desktop-menu');
 const menuOpts = document.querySelectorAll('.menu-option');
+const body = document.querySelector('body');
 
 function openMenu() {
+  body.classList.add('active');
   menu.classList.add('show-menu-modal');
 }
 
 function closeMenu() {
   menu.classList.remove('show-menu-modal');
+  body.classList.remove('active');
 }
 
 menuBtn.addEventListener('click', openMenu);
