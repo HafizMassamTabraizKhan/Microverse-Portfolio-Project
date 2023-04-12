16b0b047f6b999daf50f1beeafba49315896c808
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-04-12 13:13:54 +0500

    Add a js file for project popup and a object contatining content

---
+++ jsScripts/projectDetailWindow.js
@@ -0,0 +1,76 @@
+const works = [
+    {
+      name: 'Tonic',
+      cat: ['CANOPY', 'Back End Dev', 2015],
+      description:
+        `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and 
+        scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
+        printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text 
+        of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled 
+        it 1960s with the releawn printer took a galley of type and scrambled it 1960s.`,
+      tags: ['HTML', 'CSS', 'JavaScript'],
+      snapshot: './images/portfolio1.png',
+      connectionsBtn: ['./images/live-link-icon.png', './images/github-icon.png'],
+      connectionstext: ['See live', 'See source'],
+      connectionsURL: ['https://hafizmassamtabraizkhan.github.io/Microverse-Portfolio-Project/', 
+      'https://github.com/HafizMassamTabraizKhan/Microverse-Portfolio-Project'],
+    },
+    {
+      name: 'Multi-Post Stories',
+      cat: ['FACEBOOK', 'Full Stack Dev', 2015],
+      description:
+      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+      industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and 
+      scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
+      took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing 
+      and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the 
+      releawn printer took a galley of type and scrambled it 1960s.`,
+      tags: ['HTML', 'CSS', 'JavaScript'],
+      snapshot: './images/portfolio2.png',
+      connectionsBtn: ['./images/live-link-icon.png', './images/github-icon.png'],
+      connectionstext: ['See live', 'See source'],
+      connectionsURL: ['https://hafizmassamtabraizkhan.github.io/Microverse-Portfolio-Project/', 
+      'https://github.com/HafizMassamTabraizKhan/Microverse-Portfolio-Project'],
+    },
+    {
+      name: 'Facebook 360',
+      cat: ['FACEBOOk', 'Full Stack Dev', 2015],
+      description:
+      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+      industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and 
+      scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
+      took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing 
+      and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the 
+      releawn printer took a galley of type and scrambled it 1960s.`,
+      tags: ['HTML', 'CSS', 'JavaScript'],
+      snapshot: './images/portfolio3.png',
+      connectionsBtn: ['./images/live-link-icon.png', './images/github-icon.png'],
+      connectionstext: ['See live', 'See source'],
+      connectionsURL: ['https://hafizmassamtabraizkhan.github.io/Microverse-Portfolio-Project/', 
+      'https://github.com/HafizMassamTabraizKhan/Microverse-Portfolio-Project'],
+    },
+    {
+      name: 'Uber Navigation',
+      cat: ['Uber', 'Lead Developer', 2018],
+      description:
+      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+      industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and 
+      scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
+      took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing 
+      and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the 
+      releawn printer took a galley of type and scrambled it 1960s.`,
+      tags: ['HTML', 'CSS', 'Ruby on Rails', 'JavaScript'],
+      snapshot: './images/portfolio4.png',
+      connectionsBtn: ['./images/live-link-icon.png', './images/github-icon.png'],
+      connectionstext: ['See live', 'See source'],
+      connectionsURL: ['https://hafizmassamtabraizkhan.github.io/Microverse-Portfolio-Project/', 
+      'https://github.com/HafizMassamTabraizKhan/Microverse-Portfolio-Project'],
+    },
+  ];
+
+  
