8badd8a7a9f90c611d5da90c6ad46a465a223b76
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-04-13 14:20:41 +0500

    Add the form validation function

--- jsScripts/formValidation.js
+++ jsScripts/formValidation.js
@@ -1,1 +1,14 @@
 const errorMsg = document.getElementById('validation');
+const formEmailInput = document.querySelector('form input[type="email"]');
+const formBtn = document.querySelector('form button[type="submit"]');
+
+function emailValidation(e){
+    e.preventDefault();
+    if(formEmailInput.value !== formEmailInput.value.toLowerCase()){
+        errorMsg.textContent = 'Form not submitted. Email should be lower-cased.';
+    } else {
+        errorMsg.textContent = '';
+    }
+}
+
+formBtn.addEventListener('click', emailValidation);
