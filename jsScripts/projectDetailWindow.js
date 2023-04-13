7d68dc9c11fbbc737afbebdf8b4701ca2697ca29
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-04-13 08:18:21 +0500

    Complete project modal and work section with js object.

--- jsScripts/projectDetailWindow.js
+++ jsScripts/projectDetailWindow.js
@@ -1,7 +1,7 @@
 const works = [
     {
       name: 'Tonic',
-      cat: ['CANOPY', 'Back End Dev', 2015],
+      cat: ['CANOPY', './images/Dot.png', 'Back End Dev', './images/Dot.png', 2015],
       description:
         `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
         industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and 
@@ -19,7 +19,7 @@ const works = [
     },
     {
       name: 'Multi-Post Stories',
-      cat: ['FACEBOOK', 'Full Stack Dev', 2015],
+      cat: ['FACEBOOK', './images/Dot.png', 'Full Stack Dev', './images/Dot.png', 2015],
       description:
       `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
       industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and 
@@ -37,7 +37,7 @@ const works = [
     },
     {
       name: 'Facebook 360',
-      cat: ['FACEBOOk', 'Full Stack Dev', 2015],
+      cat: ['FACEBOOk', './images/Dot.png', 'Full Stack Dev', './images/Dot.png', 2015],
       description:
       `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
       industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and 
@@ -55,7 +55,7 @@ const works = [
     },
     {
       name: 'Uber Navigation',
-      cat: ['Uber', 'Lead Developer', 2018],
+      cat: ['Uber', './images/Dot.png', 'Lead Developer', './images/Dot.png', 2018],
       description:
       `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
       industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and 
@@ -73,25 +73,79 @@ const works = [
     },
   ];
 
-  
-const projectBtns = document.querySelectorAll('.work .work-btn');
+  const works2 = [
+    {
+      name: 'Tonic',
+      cat: ['CANOPY', './images/Dot.png', 'Back End Dev', './images/Dot.png', 2015],
+      description:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      tags: ['HTML', 'CSS', 'JavaScript'],
+      snapshot: './images/portfolio1.png',
+      connectionsBtn: ['./images/live-link-icon.png', './images/github-icon.png'],
+      connectionstext: ['See live', 'See source'],
+      connectionsURL: ['https://hafizmassamtabraizkhan.github.io/Microverse-Portfolio-Project/', 
+      'https://github.com/HafizMassamTabraizKhan/Microverse-Portfolio-Project'],
+    },
+    {
+      name: 'Multi-Post Stories',
+      cat: ['FACEBOOK', './images/Dot.png', 'Full Stack Dev', './images/Dot.png', 2015],
+      description:
+      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+      tags: ['HTML', 'CSS', 'JavaScript'],
+      snapshot: './images/portfolio2.png',
+      connectionsBtn: ['./images/live-link-icon.png', './images/github-icon.png'],
+      connectionstext: ['See live', 'See source'],
+      connectionsURL: ['https://hafizmassamtabraizkhan.github.io/Microverse-Portfolio-Project/', 
+      'https://github.com/HafizMassamTabraizKhan/Microverse-Portfolio-Project'],
+    },
+    {
+      name: 'Facebook 360',
+      cat: ['FACEBOOk', './images/Dot.png', 'Full Stack Dev', './images/Dot.png', 2015],
+      description:
+      'Exploring the future of media in Facebook\'s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+      tags: ['HTML', 'CSS', 'JavaScript'],
+      snapshot: './images/portfolio3.png',
+      connectionsBtn: ['./images/live-link-icon.png', './images/github-icon.png'],
+      connectionstext: ['See live', 'See source'],
+      connectionsURL: ['https://hafizmassamtabraizkhan.github.io/Microverse-Portfolio-Project/', 
+      'https://github.com/HafizMassamTabraizKhan/Microverse-Portfolio-Project'],
+    },
+    {
+      name: 'Uber Navigation',
+      cat: ['Uber', './images/Dot.png', 'Lead Developer', './images/Dot.png', 2018],
+      description:
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+      tags: ['HTML', 'CSS', 'Ruby on Rails', 'JavaScript'],
+      snapshot: './images/portfolio4.png',
+      connectionsBtn: ['./images/live-link-icon.png', './images/github-icon.png'],
+      connectionstext: ['See live', 'See source'],
+      connectionsURL: ['https://hafizmassamtabraizkhan.github.io/Microverse-Portfolio-Project/', 
+      'https://github.com/HafizMassamTabraizKhan/Microverse-Portfolio-Project'],
+    },
+  ];
+
+
 const bodyTag = document.querySelector('body');
+
 function projectModal(e) {
+    
   const workCardInf = works[parseInt(e.target.dataset.workbtn, 10)];
   const projectOverlay = document.createElement('div');
   projectOverlay.classList.add('project-overlay-wrapper');
+
   let modalContent = `
-  <div class= "project-overlay-wrapper">
-      <div class= "project-overlay work-card">
-          <div class="work-title-container">
+      <div class= "project-overlay project-card">
+          <div class="work-title-container project-detail">
               <h3>${workCardInf.name}</h3>
-              <span class="work-clase">X</span>
+              <span class="work-close">X</span>
           </div>
-          <ul class="work-cat">`;
+          <ul class="work-cat role">`;
               for (let i = 0; i < workCardInf.cat.length; i += 1)
               {
                   if(i === 0)
-                  modalContent += `<li class="cat-title">${workCardInf.cat[i]}</li>`;
+                  modalContent += `<li class="cat-title"><a href="#">${workCardInf.cat[i]}</a></li>`;
+                  else if(i === 1 || i === 3)
+                  modalContent += `<li class="cat-title"><img src="${workCardInf.cat[i]}" /></li>`;
                   else
                   modalContent += `<li class="cat-descrip">${workCardInf.cat[i]}</li>`;
                   
@@ -100,12 +154,12 @@ function projectModal(e) {
           `</ul>
       
           <img src="${workCardInf.snapshot}" alt="${workCardInf.name} Project, ${workCardInf.cat[2]}" class="work-image"/>
-          <div class="work-card-body">
+          <div class="work-card-body project-detail">
               <p class="work-descrip">
               ${workCardInf.description}
               </p>
               <div class="work-title-footer">
-                  <ul class="work-langs">`;
+                  <ul class="work-langs skills">`;
                       workCardInf.tags.forEach((tag) => {
                           modalContent += `<li class="work-lang">${tag}</li>`;
                       });
@@ -115,7 +169,7 @@ function projectModal(e) {
                   <div class="work-btns">`;
                       for (let i = 0; i < 2; i += 1) {
                           modalContent += 
-                          `<button class="work-btn">
+                          `<button class="work-btn btn">
                               <a href="${workCardInf.connectionsURL[i]}" target="_blank">${workCardInf.connectionstext[i]}</a> 
                               <img src="${workCardInf.connectionsBtn[i]}" alt="${workCardInf.connectionstext[i]}"/>
                           </button>`;
@@ -124,15 +178,71 @@ function projectModal(e) {
                   `</div>
               </div>
           </div>
-      </div>
-  </div>`;
+
+      </div>`;
   projectOverlay.innerHTML = modalContent;
   bodyTag.appendChild(projectOverlay);
-  const closeBtn = document.querySelector('.work-clase');
+  const closeBtn = document.querySelector('.work-close');
   const workModal = document.querySelector('.project-overlay-wrapper');
-  closeBtn.addEventListener('click', () => { 
-      body.removeChild(workModal);
-  });
+  closeBtn.addEventListener('click', () =>{
+      bodyTag.removeChild(workModal);
+    });
 }
-projectBtns.forEach((btn) => btn.addEventListener('click', projectModal));
+
+function renderworks() {
+    
+    const workSection = document.querySelector('#projects');
+    const projectOverlay = document.createElement('div');
+    projectOverlay.classList.add('project-card');
+    let cardContent = "";
+  
+    for(let i = 0; i < works2.length; i += 1) {
+        const workCardInf = works2[i];
+        const projectOverlay = document.createElement('div');
+
+        cardContent = "";
+
+        if(i === 0)
+        projectOverlay.classList.add('project-card', `card${i+1}`);
+        else
+        projectOverlay.classList.add('project-card', 'card-margin', `card${i+1}`);
+
+        cardContent += `
+        
+            <img src="${workCardInf.snapshot}" alt="${workCardInf.name} Project, ${workCardInf.cat[2]}" class="project-image">
+            <div class="project-detail">
+                <h3>${workCardInf.name}</h3>
+                <ul class="role">`;
+                    for (let i = 0; i < workCardInf.cat.length; i += 1)
+                    {
+                        if(i === 0)
+                        cardContent += `<li><a href="#">${workCardInf.cat[i]}</a></li>`;
+                        else if(i === 1 || i === 3)
+                        cardContent += `<li><img src="${workCardInf.cat[i]}" /></li>`;
+                        else
+                        cardContent += `<li>${workCardInf.cat[i]}</li>`;
+                        
+                    }
+                    cardContent += `
+                </ul>
+                <p>${workCardInf.description}</p>
+                <ul class="skills">`;
+                    workCardInf.tags.forEach((tag) => {
+                        cardContent += `<li class="work-lang">${tag}</li>`;
+                    });
+                
+                    cardContent +=
+                `</ul>
+                <button class="btn" data-workBtn="${i}">See Project</button>
+            </div>`;
+
+        projectOverlay.innerHTML = cardContent;
+        workSection.appendChild(projectOverlay);
+    }
+
+    const projectBtns = document.querySelectorAll('.project-card .btn');
+    projectBtns.forEach((btn) => btn.addEventListener('click', projectModal));
+}  
+
+window.addEventListener('load', renderworks);
   
